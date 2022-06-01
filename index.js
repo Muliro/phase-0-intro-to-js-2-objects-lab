957b1221cdb90a3f51a7cbb3bdd87e377605ba87
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-01 17:36:53 +0300

    Javascript objects

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+const employee = {"name": "Michael Muliro", "streetAdress": "Runda"};
+function updateEmployeeWithKeyAndValue(emp, key, value){
+    return {...emp, [key]:value,};
+}
+
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key){
+    const newObj= {...obj};
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key];
+    return obj;
+}
